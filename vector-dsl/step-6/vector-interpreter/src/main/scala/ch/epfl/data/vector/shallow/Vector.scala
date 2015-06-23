df3c74bae890635c898bb653fef890bdd786f5e0
amirsh <amir.shaikhha@epfl.ch>
2015-06-23 14:23:59 +0200

    Requirement on Int should be changed to Numeric[T]

--- vector-dsl/step-6/vector-interpreter/src/main/scala/ch/epfl/data/vector/shallow/Vector.scala
+++ vector-dsl/step-6/vector-interpreter/src/main/scala/ch/epfl/data/vector/shallow/Vector.scala
@@ -7,7 +7,7 @@ import scala.collection.mutable.IndexedSeq
 
 @deep
 @transformation
-@needs[(scala.runtime.RichInt, Seq[_], Int, IndexedSeq[_])]
+@needs[(scala.runtime.RichInt, Seq[_], Numeric[_], IndexedSeq[_])]
 class Vector(val data: Seq[Int]) {
   @pure def +(v2: Vector): Vector = {
     val resultData = for (i <- 0 until data.size) yield (data(i) + v2.data(i))
