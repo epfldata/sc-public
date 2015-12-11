616694dfa00ed96e9be0e4c4f1c4d1f3218a5d5b
Amir Shaikhha <amir.shaikhha@epfl.ch>
2015-12-11 16:52:34 +0100

    All cake mixins are performed by Purgatory using annotations.

--- newtuto/mylib-deep/src/main/scala/mylib/deep/MyLibDSLs.scala
+++
@@ -1,7 +0,0 @@
-package mylib.deep
-
-import ch.epfl.data.sc.pardis.quasi.anf.{BaseQuasiExp, BaseQuasiExt}
-
-class MyLibDSLOps extends MyLibOps with BaseQuasiExp
-
-class MyLibDSLExtOps extends MyLibExtOps with BaseQuasiExt
--- newtuto/src/main/scala/mylib/shallow/MyLibDSL.scala
+++ newtuto/src/main/scala/mylib/shallow/MyLibDSL.scala
@@ -4,7 +4,8 @@ package shallow
 import ch.epfl.data.sc.pardis.annotations._
 import ch.epfl.data.sc.pardis.shallow.scalalib._
 
+@quasiquotation
 @deep
 @deepExt
 @needs[List[_] :: ScalaCore]
-trait MyLib
+trait MyLibDSL
