e045c05f25e91b0df95e64d007af16c965fa31b3
Amir Shaikhha <amir.shaikhha@epfl.ch>
2015-12-16 12:36:10 +0100

    more clean up for memory management

--- newtuto/mylib-deep/src/main/scala/mylib/compiler/CGenLowering.scala
+++ newtuto/mylib-deep/src/main/scala/mylib/compiler/CGenLowering.scala
@@ -29,10 +29,15 @@ class CGenLowering(override val IR: MyLibDSLOps) extends RecursiveRuleBasedTrans
   case class Arr[T](arr: Rep[Array[T]])(implicit val tp: TypeRep[T])
 
   var arrays = mutable.ArrayBuffer[Arr[_]]()
+
+  def postProcessBlock[T](b: Block[T]): Unit = {
+    arrays foreach {
+        case a => // Note: a => doesn't work (for very obscure Scala reasons)
+          import a.tp
+          dsl"Mem.free(${a.arr})"
+      }
+  }
   
-  val arraySizes = mutable.Map[Rep[_], IR.Var[Int]]()
-  
-  //override def transformBlock[T: TypeRep](b: Block[T]) = { // doesn't work; FIXME remove this confusing one
   override def transformBlockTyped[T: TypeRep, S: TypeRep](b: Block[T]): to.Block[S] = {
     
     val oldArrays = arrays
@@ -43,17 +48,12 @@ class CGenLowering(override val IR: MyLibDSLOps) extends RecursiveRuleBasedTrans
         transformStm(s)
       }
       
-      arrays foreach {
-        case a => // Note: a => doesn't work (for very obscure Scala reasons)
-          import a.tp
-          dsl"Mem.free(${a.arr})"
-      }
+      postProcessBlock(b)
       
       apply(b.res)
     }
     arrays = oldArrays
     nb.asInstanceOf[Block[S]]
   }
-  
   
 }
