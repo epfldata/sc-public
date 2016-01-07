f080751913794f563834ab621ace1f3e33da9c4c
LPTK <lionel.parreaux@gmail.com>
2016-01-07 13:01:38 +0100

    More spacing

--- list-dsl/list-deep/src/main/scala/list/compiler/Main.scala
+++ list-dsl/list-deep/src/main/scala/list/compiler/Main.scala
@@ -51,8 +51,10 @@ object Main extends App {
   
   {
     import Context.Predef._  // needed to provide the `compile` methods with an implicit TypeRep
+    
     // Creates the directories if do not already exist!
     new java.io.File("generator-out/src/main/scala").mkdirs()
+    
     new MyCompiler(Context, "GenApp", offlineOptim = false).compile(pgrm, "src/main/scala/GenApp")
     new MyCompiler(Context, "GenAppOpt", offlineOptim = true).compile(pgrm, "src/main/scala/GenAppOpt")
     new MyCompiler(Context, "GenAppLow", lowering = 1).compile(pgrm, "src/main/scala/GenAppLow")
