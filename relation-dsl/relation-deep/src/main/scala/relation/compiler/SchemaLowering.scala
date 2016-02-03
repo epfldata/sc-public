c60bf39f902599f8b36f317be3113b5d2158135c
amirsh <amir.shaikhha@epfl.ch>
2016-02-03 17:08:01 +0100

    better formatting

--- relation-dsl/relation-deep/src/main/scala/relation/compiler/SchemaLowering.scala
+++ relation-dsl/relation-deep/src/main/scala/relation/compiler/SchemaLowering.scala
@@ -21,94 +21,94 @@ class SchemaLowering(override val IR: RelationDSLOpsPackaged) extends RecursiveR
   import IR.{__new, field, __lambda}
 
   def convertToStaticSchema(schema: Rep[Schema]): Schema = {
-  	schema match {
-  		case dsl"Schema($xs*)" => 
-  		  val names = for(Constant(x) <- xs) yield x
-  		  new Schema(names.toList)
-  	}
+    schema match {
+      case dsl"Schema($xs*)" => 
+        val names = for(Constant(x) <- xs) yield x
+        new Schema(names.toList)
+    }
   }
 
   private var recordsCount = 0
 
   def getClassTag = {
-  	recordsCount += 1
-  	StructTags.ClassTag(s"Record$recordsCount")
+    recordsCount += 1
+    StructTags.ClassTag(s"Record$recordsCount")
   }
 
   class Rec
 
   rewrite += symRule {
-  	case dsl"Relation.scan(${Constant(fileName)}, $schema, ${Constant(delimiter)})" => 
-  		val constantSchema = convertToStaticSchema(schema)
-  		val scanner = dsl"new RelationScanner($fileName, ${delimiter.charAt(0)})"
+    case dsl"Relation.scan(${Constant(fileName)}, $schema, ${Constant(delimiter)})" => 
+      val constantSchema = convertToStaticSchema(schema)
+      val scanner = dsl"new RelationScanner($fileName, ${delimiter.charAt(0)})"
 
-  		implicit val recTp: TypeRep[Rec] = new RecordType[Rec](getClassTag, None)
-  		def loadRecord: Rep[Rec] = __new[Rec](constantSchema.columns.map(column => (column, false, dsl"$scanner.next_string()")): _*)
+      implicit val recTp: TypeRep[Rec] = new RecordType[Rec](getClassTag, None)
+      def loadRecord: Rep[Rec] = __new[Rec](constantSchema.columns.map(column => (column, false, dsl"$scanner.next_string()")): _*)
 
-  		dsl"""
-  		    val arr = new Array[Rec](3)
-  		    var i = 0
-  			while($scanner.hasNext) {
-  				val rec = $loadRecord
-  				arr(i) = rec
-  				i = i + 1
-  			}
-  			arr
-  		""".asInstanceOf[Rep[Any]]
+      dsl"""
+          val arr = new Array[Rec](3)
+          var i = 0
+        while($scanner.hasNext) {
+          val rec = $loadRecord
+          arr(i) = rec
+          i = i + 1
+        }
+        arr
+      """.asInstanceOf[Rep[Any]]
   }
 
   rewrite += symRule {
-  	case dsl"(${ArrFromRelation(arr)}: Relation).project($schema)" => 
-  		val constantSchema = convertToStaticSchema(schema)
+    case dsl"(${ArrFromRelation(arr)}: Relation).project($schema)" => 
+      val constantSchema = convertToStaticSchema(schema)
 
-  		implicit val recTp: TypeRep[Rec] = new RecordType[Rec](getClassTag, None)
-  		def copyRecord(e: Rep[Any]): Rep[Rec] = __new[Rec](constantSchema.columns.map(column => (column, false, field[String](e, column))): _*)
-  		val newArr = dsl"new Array[Rec]($arr.length)"
-  		import IR.RangeRep
-  		IR.Range(dsl"0", dsl"$arr.length").foreach(__lambda({ (j: Rep[Int]) => 
-			val e = dsl"$arr($j)"
-  			dsl"$newArr($j) = ${copyRecord(e)}"
-  		}))
-		newArr.asInstanceOf[Rep[Any]]
+      implicit val recTp: TypeRep[Rec] = new RecordType[Rec](getClassTag, None)
+      def copyRecord(e: Rep[Any]): Rep[Rec] = __new[Rec](constantSchema.columns.map(column => (column, false, field[String](e, column))): _*)
+      val newArr = dsl"new Array[Rec]($arr.length)"
+      import IR.RangeRep
+      IR.Range(dsl"0", dsl"$arr.length").foreach(__lambda({ (j: Rep[Int]) => 
+      val e = dsl"$arr($j)"
+        dsl"$newArr($j) = ${copyRecord(e)}"
+      }))
+    newArr.asInstanceOf[Rep[Any]]
   }
 
   rewrite += symRule {
-  	case dsl"(${ArrFromRelation(arr)}: Relation).select((x: Row) => x.getField($name) == ($value: String))" => {
-  		implicit val recTp: TypeRep[Rec] = arr.tp.typeArguments(0).asInstanceOf[TypeRep[Rec]]
-  		dsl"""
-  			var size = 0
-  			Range(0, $arr.length).foreach({ j => 
-  				val e = $arr(j)
-  				if(__struct_field[String](e, $name) == $value) {
-  					size = size + 1
-  				}
-  			})
-  			val arr = new Array[Rec](size)
-  			var i = 0
-  			Range(0, arr.length).foreach({ j => 
-  				val e = $arr(j)
-  				if(__struct_field[String](e, $name) == $value) {
-  					arr(i) = $arr(j)
-  					i = i + 1
-  				}
-  			})
-  			arr
-  		"""
-  		// dsl"println($value)"
-  	}
+    case dsl"(${ArrFromRelation(arr)}: Relation).select((x: Row) => x.getField($name) == ($value: String))" => {
+      implicit val recTp: TypeRep[Rec] = arr.tp.typeArguments(0).asInstanceOf[TypeRep[Rec]]
+      dsl"""
+        var size = 0
+        Range(0, $arr.length).foreach({ j => 
+          val e = $arr(j)
+          if(__struct_field[String](e, $name) == $value) {
+            size = size + 1
+          }
+        })
+        val arr = new Array[Rec](size)
+        var i = 0
+        Range(0, arr.length).foreach({ j => 
+          val e = $arr(j)
+          if(__struct_field[String](e, $name) == $value) {
+            arr(i) = $arr(j)
+            i = i + 1
+          }
+        })
+        arr
+      """
+      // dsl"println($value)"
+    }
   }
 
   rewrite += symRule {
-  	case dsl"(${ArrFromRelation(arr)}: Relation).print" => {
-  		implicit val recTp: TypeRep[Rec] = arr.tp.typeArguments(0).asInstanceOf[TypeRep[Rec]]
-  		dsl"""
-  			Range(0, $arr.length).foreach({ j => 
-  				val e = $arr(j)
-  				println(e)
-  			})
-  		"""
-  		// dsl"println($value)"
-  	}
+    case dsl"(${ArrFromRelation(arr)}: Relation).print" => {
+      implicit val recTp: TypeRep[Rec] = arr.tp.typeArguments(0).asInstanceOf[TypeRep[Rec]]
+      dsl"""
+        Range(0, $arr.length).foreach({ j => 
+          val e = $arr(j)
+          println(e)
+        })
+      """
+      // dsl"println($value)"
+    }
   }
 
   object ArrFromRelation {
--- relation-dsl/src/test/scala/relation/shallow/SimpleQuery.scala
+++ relation-dsl/src/test/scala/relation/shallow/SimpleQuery.scala
@@ -5,38 +5,38 @@ import org.scalatest._
 import Matchers._
 
 class SimpleQuery extends FlatSpec {
-	val RSchema = new Schema(List("number", "digit"))
-	val RFile = "data/R.csv"
-	val RScan = Relation.scan(RFile, RSchema, "|")
+  val RSchema = new Schema(List("number", "digit"))
+  val RFile = "data/R.csv"
+  val RScan = Relation.scan(RFile, RSchema, "|")
 
-	"Scanning R" should "work" in {
-		RScan.toString should be {
+  "Scanning R" should "work" in {
+    RScan.toString should be {
 """one, 1
 two, 2
 three, 3
 """
-		}
-	}
+    }
+  }
 
-	val RSelect = RScan.select(r => r.getField("digit").toInt % 2 == 1)
+  val RSelect = RScan.select(r => r.getField("digit").toInt % 2 == 1)
 
-	"Selecting R" should "work" in {
-		RSelect.toString should be {
+  "Selecting R" should "work" in {
+    RSelect.toString should be {
 """one, 1
 three, 3
 """
-		}
-	}
+    }
+  }
 
-	val RPSchema = new Schema(List("number"))
-	val RProject = RScan.project(RPSchema)
+  val RPSchema = new Schema(List("number"))
+  val RProject = RScan.project(RPSchema)
 
-	"Projecting R" should "work" in {
-		RProject.toString should be {
+  "Projecting R" should "work" in {
+    RProject.toString should be {
 """one
 two
 three
 """
-		}
-	}
+    }
+  }
 }
