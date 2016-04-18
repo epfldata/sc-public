799b0e76585c5976de787486211ed3a8f0850a08
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-04-19 01:04:33 +0200

    RelationCompiler uses the improved SC generators

--- relation-dsl/relation-deep/src/main/scala/relation/compiler/RelationCompiler.scala
+++ relation-dsl/relation-deep/src/main/scala/relation/compiler/RelationCompiler.scala
@@ -2,17 +2,17 @@ package relation
 package compiler
 
 import ch.epfl.data.sc.pardis
-import ch.epfl.data.sc.pardis.deep.scalalib.ArrayIRs.Array_Field__length
+import pardis.deep.scalalib.ArrayScalaCodeGen
 import pardis.optimization._
 import pardis.compiler._
 import pardis.ir._
-import pardis.deep.scalalib.ScalaCoreCCodeGen
+import pardis.utils.document._
+import pardis.prettyprinter._
 import deep._
 
 class RelationCompiler(val DSL: RelationDSLOpsPackaged, name: String) extends Compiler[RelationDSLOpsPackaged] {
   
   // Pipeline Definition:
-  
   pipeline += DCE
 
   val schemaAnalysis = new SchemaAnalysis(DSL)
@@ -26,25 +26,9 @@ class RelationCompiler(val DSL: RelationDSLOpsPackaged, name: String) extends Co
   
   // Outputting Scala code inside an executable wrapper:
   
-  import pardis.prettyprinter._
-  
   val codeGenerator = 
-      new ScalaCodeGenerator with ASTCodeGenerator[RelationDSLOpsPackaged] {
+      new ScalaCodeGenerator with ASTCodeGenerator[RelationDSLOpsPackaged] with ArrayScalaCodeGen {
         val IR = DSL
-        import pardis.utils.document._
-          override def nodeToDocument(node: PardisNode[_]): Document = {
-    node match {
-      case PardisFor(start, end, step, variable, block)                    => 
-        val stepDoc = step match {
-          case Constant(1) => doc""
-          case _ => doc" by $step"
-        }
-        doc"for($variable <- $start until $end$stepDoc) ${blockToDocument(block)}"
-      case Array_Field__length(self)                    => 
-        doc"$self.length"
-      case _ => super.nodeToDocument(node)
-    }
-  }
         override def header(): Document = s"""
           |package relation
           |import relation.shallow._""".stripMargin
